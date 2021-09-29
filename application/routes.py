bca8d1a6a995cf1b29a94ebc2a2e932bdb206284
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-29 10:46:55 -0400

    feat(titles): added dynamic titles

--- application/routes.py
+++ application/routes.py
@@ -3,8 +3,8 @@ from application import app
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="Home")
 
 @app.route('/references')
 def references():
-    return render_template("references.html")
+    return render_template("references.html", title="References")
