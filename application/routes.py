40999c7e0cd2db2941e6733e713e29625e6eae16
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-29 10:07:07 -0400

    feat(references): added references

--- application/routes.py
+++ application/routes.py
@@ -4,3 +4,7 @@ from application import app
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/references')
+def references():
+    return render_template("references.html")
